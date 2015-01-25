61b59184e208f6762531b1983ba1c2260c70e4ed
Alexey Plutalov <demiazz.py@gmail.com>
2015-01-25 03:55:34 +0300

    Move scripts to templates. Replace vagrant on erb in vagrant.sh

---
+++ templates/scripts/vagrant.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir ~<%= username %>/.ssh
+wget --no-check-certificate \
+'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' \
+-O ~%<= username %>/.ssh/authorized_keys
+chown -R %<= username %> ~%<= username %>/.ssh
+chmod -R go-rwsx ~%<= username %>/.ssh
