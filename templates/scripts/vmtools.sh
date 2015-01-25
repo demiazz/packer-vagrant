b61f1c0dbfc77a031ceb36dc1ca71e6dd5b7ccd1
Alexey Plutalov <demiazz.py@gmail.com>
2015-01-25 15:03:43 +0300

    Fix templates

--- templates/scripts/vmtools.sh
+++ templates/scripts/vmtools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir /tmp/parallels
-mount -o loop /home/vagrant/prl-tools-lin.iso /tmp/parallels
+mount -o loop /home/<%= username %>/prl-tools-lin.iso /tmp/parallels
 /tmp/parallels/install --install-unattended-with-deps
 umount /tmp/parallels
 rmdir /tmp/parallels
-rm /home/vagrant/*.iso
+rm /home/<%= username %>/*.iso
